6e3ba384136a87e3043e1f989846810234edc691
Ni1esh-Yadav <yadav.nilesh9172@gmail.com>
2025-09-02 07:05:33 +0530

    removed unnecessary otpverification logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,6 @@ import { AuthProvider, AuthContext } from "./context/AuthContext";
 
 import Signup from "./pages/Auth/Signup";
 import Login from "./pages/Auth/Login";
-import OtpVerification from "./pages/Auth/OtpVerification";
 import Notes from "./pages/Notes/Notes";
 
 const ProtectedRoute: React.FC<{ children: React.JSX.Element }> = ({
@@ -29,10 +28,9 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/" element={<Navigate to="/signup" replace />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/verify-otp" element={<OtpVerification />} />
 
           <Route
             path="/notes"
